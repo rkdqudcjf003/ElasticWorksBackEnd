ddaff6230f30ea7be4b3615d4122454c84c89451
jwkang5665 <jwkang5665@gmail.com>
2021-09-01 14:27:23 +0900

    admincontroller 수정중 application.yml 수정

--- ElasticWorksBackEnd/src/main/java/kr/co/elasticworks/api/controller/AdminController.java
+++ ElasticWorksBackEnd/src/main/java/kr/co/elasticworks/api/controller/AdminController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,63 +33,57 @@ public class AdminController {
 		return "굿";
 	}
 	
-	//전체 boardList 출력
-	@RequestMapping(value="/boardList")
-		public Map<String, Object> boardList(@RequestParam int page_no,
-				@RequestParam String keyword,
-				@RequestParam String type) throws Exception {
-			Map<String, Object> result = new HashMap<>();
-			SearchPagingUtil search = new SearchPagingUtil();
-			
-			if (page_no <= 0)
-				page_no = 1;
-			
-			if (keyword == null)
-				keyword = "";
-			
-			if (type == null)
-				type = "";
+	//(전체 or 카테고리별) and (검색키워드 or 검색유형별) list 출력
+	@GetMapping(value = "/boardList")
+	public Map<String, Object> boardList(@RequestParam(value = "page_no", required = false) int page_no,
+			@RequestParam(value = "keyword", required = false) String keyword,
+			@RequestParam(value = "type", required = false) String type,
+			@RequestParam(value = "category", required = false) int category) throws Exception {
+		Map<String, Object> result = new HashMap<>();
+		SearchPagingUtil search = new SearchPagingUtil();
 
-			search.setCurrentPageNo(page_no);
-			search.setSearchKeyword(keyword);
-			search.setSearchType(type);
-			
-			int boardTotalCount = boardService.getBoardListCnt(search);
+		if (page_no <= 0)
+			page_no = 1;
 
-			
-			search.setTotalBoardCount(boardTotalCount);
+		if (keyword == null)
+			keyword = "";
 
-			List<Board> boardList = null;
-			
-			if (boardTotalCount > 0) {
-				boardList = boardService.getBoardList(search);
+		if (type == null)
+			type = "";
+		
+		if (category <= 0)
+			category = 0;
+		
+		search.setCurrentPageNo(page_no);
+		search.setSearchKeyword(keyword);
+		search.setSearchType(type);
+		
+		int boardTotalCount = 0;
+		
+		if (category == 0) {
+			boardTotalCount = boardService.getAllBoardListCnt(search);
+			System.out.println("전체전체");
+		} else if (category > 0){
+			search.setCategory(category);
+			boardTotalCount = boardService.getCategoryBoardListCnt(search);
+			System.out.println("카테고리");
+		}
+		System.out.println(boardTotalCount);
+		search.setTotalBoardCount(boardTotalCount);
 
-			}
+		List<Board> boardList = null;
 
-			result.put("pageInfo", search);
-			result.put("boardList", boardList);
+		if (boardTotalCount > 0 && category == 0) {
+			boardList = boardService.getAllBoardList(search);
+		} else if (boardTotalCount > 0 && category > 0) {
+			search.setCategory(category);
+			boardList = boardService.getCategoryBoardList(search);
+		}
 
-			return result;
+		result.put("pageInfo", search);
+		result.put("boardList", boardList);
+
+		return result;
 	}
 	
-	//noticeBoardList출력
-	@RequestMapping(value="/noticeBoardList")
-	public String noticeBoardList() {
-		System.out.println("noticeBoardListController실행");
-		return "굿";
-	}
-	
-	//faqBoardList출력
-	@RequestMapping(value="/faqBoardList")
-	public String faqBoardList() {
-		System.out.println("faqBoardListController실행");
-		return "굿";
-	}
-	
-	//qnaBoardList출력
-	@RequestMapping(value="/qnaBoardList")
-	public String qnaBoardList() {
-		System.out.println("qnaBoardListController실행");
-		return "굿";
-	}
 }
